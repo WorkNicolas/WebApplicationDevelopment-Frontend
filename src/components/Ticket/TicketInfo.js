c56d36d892268fbc1fd017a3e19780e69f80a82b
stephen-hui <huilt1232@gmail.com>
2024-11-21 18:20:22 -0500

    add input comment

--- src/components/Ticket/TicketInfo.js
+++ src/components/Ticket/TicketInfo.js
@@ -1,26 +1,27 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import CommentBox from "./CommentBox";
+import Avatar from "./Avatar";
 
 const TicketInfo = () => {
   const { id } = useParams();
-
+  const [newComment, setNewComment] = useState("");
   const [ticket, setTicket] = useState(null);
   const [comments, setComments] = useState([
     {
-        username: "Stephen Hui",
-        comment: "This is a comment",
-        createdAt: "2021-01-01"
+      username: "Stephen Hui",
+      comment: "This is a comment",
+      createdAt: "2021-01-01",
     },
     {
-        username: "Stephen Hui",
-        comment: "This is a comment",
-        createdAt: "2021-01-01"
+      username: "Stephen Hui",
+      comment: "This is a comment",
+      createdAt: "2021-01-01",
     },
     {
-        username: "Stephen Hui",
-        comment: "This is a comment",
-        createdAt: "2021-01-01"
+      username: "Stephen Hui",
+      comment: "This is a comment",
+      createdAt: "2021-01-01",
     },
   ]);
 
@@ -67,15 +68,26 @@ const TicketInfo = () => {
         <span className="p-3 border-bottom bg-light text-md h4">
           {"Comments"}
         </span>
+        <div className="p-3 d-flex gap-4 w-100 mb-4 ">
+          <Avatar username={"Stephen"} />
+          <input
+            type="text"
+            placeholder="Leave a comment"
+            className="w-75 p-2"
+            value={newComment}
+            onChange={(e) => setNewComment(e.target.value)}
+          />
+          <button className="btn btn-outline-secondary">Post</button>
+        </div>
         <div className="p-3 d-flex flex-column gap-4 my-4">
-            {comments.map((comment, index) => (
-                <CommentBox
-                key={index}
-                username={comment.username}
-                comment={comment.comment}
-                createdAt={comment.createdAt}
-                />
-            ))}
+          {comments.map((comment, index) => (
+            <CommentBox
+              key={index}
+              username={comment.username}
+              comment={comment.comment}
+              createdAt={comment.createdAt}
+            />
+          ))}
         </div>
       </div>
     </div>
